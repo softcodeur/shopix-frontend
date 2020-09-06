3b879424bae83b1e3e3c02425f30367798d51f60
yassir acaf <yassir.acaf@gmail.com>
2020-09-06 17:11:02 +0100

    last commit

--- src/app/services/adresse.service.ts
+++ src/app/services/adresse.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {User} from "../models/user";
-import {Adresse} from "../models/adresse";
-const api_url ="https://shopix-backend.herokuapp.com";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../models/user';
+import { Adresse } from '../models/adresse';
+const api_url = 'https://shopix-backend.herokuapp.com';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdresseService {
-
-  constructor(private http:HttpClient) { }
-  addAdresse(user:User,adresse:Adresse){
+  constructor(private http: HttpClient) {}
+  addAdresse(user: User, adresse: Adresse) {
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type':  'application/json'
-      })};
-    return this.http.post(`${api_url}/shopix-api/adresse/email/${user.email}/password/${user.password}`,adresse,httpOptions);
-
-
-
+        'Content-Type': 'application/json',
+      }),
+    };
+    return this.http.post(
+      `${api_url}/shopix-api/adresse/email/${user.email}/password/${user.password}`,
+      adresse,
+      httpOptions
+    );
   }
 }
